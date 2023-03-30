48f46a864becc320ec355cac475127ba3b34db71
Justin Pasko <jpasko1112@gmail.com>
2023-03-29 22:02:12 -0400

    Add several features and tests.

--- lib/team.rb
+++ lib/team.rb
@@ -1,8 +1,38 @@
 class Team
-  attr_reader :name, :city
+  attr_reader :name, :city, :roster, :player_count
   
   def initialize(name, city)
     @name = name
     @city = city
+    @roster = []
+    @player_count = 0
+  end
+
+  def add_player(player)
+    @roster << player
+    @player_count += 1
+  end
+
+  def long_term_players
+    @roster.select do |player|
+      player.contract_length >24
+    end
+  end
+
+  def short_term_players
+    @roster.select do |player|
+      player.contract_length <= 24
+    end
+  end
+
+  def total_value
+    @roster.sum do |player|
+      player.total_cost
+    end
+  end
+
+  def details
+    {"total_value" => total_value, "player_count"=> @player_count}
   end
 end
+
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,9 +2,68 @@ require './lib/player'
 require './lib/team'
 
 RSpec.describe Team do
-  it 'exists' do
-    team = ("Dodgers", "Los Angeles")
+  before(:each) do
+    @team = Team.new("Dodgers", "Los Angeles")
+    @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    @player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    @player_3 = Player.new("Alan McClennan", 750000, 48)
+    @player_4 = Player.new("Hamilton Porter", 100000, 12)
+  end
 
-    expect(team).to be_a Team
+  it 'exists' do
+    expect(@team).to be_a Team
+  end
+
+  it "has a roster" do
+    expect(@team.roster).to eq([])
+  end
+
+  it "has a player count" do
+    expect(@team.player_count).to eq(0)
+  end
+
+  it "adds a player to the roster" do
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+    
+    expect(@team.roster).to eq([@player_1, @player_2])
+    expect(@team.player_count).to eq(2)
+  end
+
+  it "has long term players" do
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+    @team.add_player(@player_3)
+    @team.add_player(@player_4)
+
+    expect(@team.long_term_players).to eq([@player_1, @player_3])
+  end
+
+  it "has short term players" do
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+    @team.add_player(@player_3)
+    @team.add_player(@player_4)
+
+    expect(@team.short_term_players).to eq([@player_2, @player_4])
+  end
+
+  it "has a total value" do
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+    @team.add_player(@player_3)
+    @team.add_player(@player_4)
+
+    expect(@team.total_value).to eq(85200000)
+  end
+
+  it "has details" do
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+    @team.add_player(@player_3)
+    @team.add_player(@player_4)
+
+    expected = {"total_value" => 85200000, "player_count" => 4}
+    expect(@team.details).to eq(expected)
   end
 end
